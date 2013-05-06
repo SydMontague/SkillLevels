ac1027b2fae9e52444171d72c2dfbbab190a626c
SydMontague <sydmontague@web.de>
2013-05-06 17:24:21 +0200

    changed name

--- src/de/craftlancer/skilllevels/SkillLevels.java
+++ src/de/craftlancer/skilllevels/SkillLevels.java
@@ -8,8 +8,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SkillLevels extends JavaPlugin
 {
-    FileConfiguration config;
-    FileConfiguration pconfig;
+    private FileConfiguration config;
+    private FileConfiguration pconfig;
     
     @Override
     public void onEnable()
